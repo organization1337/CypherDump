a41920f7ea10e21ceca16e088045d7635a8e8456
Idan Steinberg <steinberg.idan@gmail.com>
2019-02-02 17:41:32 +0200

    no message

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log('Hello World');
+#!/usr/bin/env node
+import { CommanderStatic } from "commander";
+
+const program: CommanderStatic = require('commander');
+
+program
+    .version('1.0.0')
+    .command('install')
+    .parse(process.argv);
